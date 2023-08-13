257b0e3ea801754aff9427e03c304461ae3777cf
LeFab70 <kouonang2002@gmail.com>
2023-08-13 12:25:12 -0400

    initial

---
+++ src/main/java/dockercompose/reservations/GestionDesReservationsApplication.java
@@ -0,0 +1,13 @@
+package dockercompose.reservations;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionDesReservationsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionDesReservationsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dockercompose/reservations/GestionDesReservationsApplicationTests.java
@@ -0,0 +1,13 @@
+package dockercompose.reservations;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionDesReservationsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
